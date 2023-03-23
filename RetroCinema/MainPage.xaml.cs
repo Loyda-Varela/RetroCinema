bd28eb4d92ebc0406bc8a48073ddffee3af266f9
Loyda-Varela <73678119+Loyda-Varela@users.noreply.github.com>
2023-03-22 23:20:36 -0600

    Add project files.

---
+++ RetroCinema/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RetroCinema
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInSecion_Clicked(object sender, EventArgs e)
+        {
+            if (usuario.Text=="admin" && contraseña.Text == "123")
+                {
+                    Navigation.PushAsync(new Pages.Home());
+                }
+               
+        }
+
+        
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pages.PageRegistro());
+        }
+    }
+}
---
+++ RetroCinema/Pages/PageRegistro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RetroCinema.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageRegistro : ContentPage
+    {
+        public PageRegistro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
